ebbc7e904dd12a6b537ecf1ae4288f3f4889287b
mercuryih <63509755+mercuryih@users.noreply.github.com>
2020-12-22 00:17:19 +0200

    Add files via upload

---
+++ DayBook/DBConn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace DayBook
+{
+    public class DBConn
+    {
+        public static SqlConnection Connection { get; private set; }
+        private DBConn() { }
+        public static void SetConnection(string connStr)
+        {
+            if(Connection == null)
+                Connection = new SqlConnection(connStr);
+        }
+    }
+}
---
+++ DayBook/DBOperations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace DayBook
+{
+    class DBOperations
+    {
+        public void DBAddNewItem(string date, string data)
+        {
+            SqlConnection connection = DBConn.Connection;
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand();
+                command.CommandText = $"INSERT INTO (Date, Task) VALUES ('{date}', '{data}')";
+                command.Connection = connection;
+                command.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                throw new Exception("Problem with database");
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+    }
+}
